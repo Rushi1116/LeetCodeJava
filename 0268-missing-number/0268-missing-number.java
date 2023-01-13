e1bd57fe05e5693f6f90207bd2fb4f0a02a26133
Rushikesh Kalane <118335819+Rushi1116@users.noreply.github.com>
2023-01-14 00:03:53 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (67.83%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+         int n=nums.length,totalsum=n*(n+1)/2; //sum of first n natural numbers
+        for(int i:nums)
+            totalsum-=i;
+        return totalsum;
+    }
+}
